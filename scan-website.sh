807f5cc23f451d3e8eb38f02eb48c02679c91518
Roman Gusev <rgusev2@gmail.com>
2024-06-06 13:36:12 +0200

    Remove text scan

--- scan-website.sh
+++ scan-website.sh
@@ -1,14 +1,8 @@
 website_url=$1
-scan_text=$2
 status_code=$(curl --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
 
 if [ "${status_code}" != "200" ]; then
     echo "Website is unreachable (${status_code})" >&2
-    exit 1
-fi
-
-if ! curl --silent --location "{$website_url}" | grep --quiet --ignore-case "${scan_text}"; then
-    echo "Text was not found on website" >&2
     exit 1
 fi
 
